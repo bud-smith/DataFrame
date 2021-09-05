6aee03c2f4acefcb7169b90ac9291c29ad5658f6
bud-smith <sambarino135@gmail.com>
2021-09-05 15:46:58 +1000

    Continued progression

--- 3802ICT - Assessment 2.cpp
+++ 3802ICT - Assessment 2.cpp
@@ -7,39 +7,39 @@
 
 using namespace std;
 
-template<typename T>
+template<template<typename> typename Column, typename T>
 class DataFrame {
 private:
 	// data_vec is the storage of the data frames.
-	vector<vector<T>> data_vec;
+	vector<Column<T>> data_vec;
 public:
 	// 1.1 - Initialisation
 	// Default constructor
 	DataFrame() {};
 	// Parameterised constructor
-	DataFrame(vector<vector<T>> vectors) {
-		data_vec = vectors;
+	DataFrame(vector<Column<T>> _vectors) {
+		data_vec = _vectors;
 	};
 
 	// 1.2 - Load Data
 	// Loads vectors data into data_vec and overwrites original data
-	void load_data(vector<vector<T>> _vectors) {
+	void load_data(vector<Column<T>> _vectors) {
 		data_vec = _vectors;
 	}
-	void load_data(vector<T> _vector) {
+	void load_data(Column<T> _vector) {
 		data_vec = _vector;
 	}
 	// Adds data to the data_vec, that being a column of data.
-	void add_data(vector<T> _vector) {
+	void add_data(Column<T> _vector) {
 		data_vec.push_back(_vector);
 	}
-	void add_data(vector<vector<T>> _vectors) {}
-	void add_data(vector<T> _vector, string column_name) {}
-	void add_data(vector<vector<T>> _vectors, string column_name) {}
+	void add_data(vector<Column<T>> _vectors) {}
+	void add_data(Column<T> _vector, string column_name) {}
+	void add_data(vector<Column<T>> _vectors, string column_name) {}
 
 	// 1.3 - Setting Column Names
 	void set_columns(string column_names[], uint32_t size) {
-		for (size_t i = 0; i < size; i++) {
+		for (int32_t i = 0; i < size; i++) {
 			data_vec[0][i] = column_names[i];
 		}
 	}
@@ -53,9 +53,9 @@ public:
 	void update_columnval(string column_name, vector<T> _vector) {}
 	void delete_column(uint32_t column_idx) {}
 	void delete_column(string column_name) {}
-	
+
 	// 1.6 - Get Columns
-	vector<T> &operator[](uint32_t column_idx) {
+	Column<T>& operator[](uint32_t column_idx) {
 		return data_vec[column_idx];
 	}
 
@@ -67,25 +67,27 @@ public:
 		return (data_vec[0].size());
 	}
 
-	// 1.8 - Statistical Measures
-	void min() {}
-	void max() {}
-	void mean() {}
-	void median() {}
-	void mode() {}
-	void summary() {}
-
-	vector<vector<T>> get_dataframe() {
+	vector<Column<T>> get_dataframe() {
 		return data_vec;
 	}
 };
 
 template<typename T>
-class Column {
+class Column : public vector<T> {
 private:
-	vector<T> column_data;
+	vector<T> data_vector;
 public:
-	void min() {}
+	using vector<T>::vector;
+
+	Column(vector<T> _vector) {
+		vector = data_vector;
+	}
+
+	void min() {
+		for (int32_t i = 0; i < data_vector.size(); i++) {
+			cout << data_vector[i] << " ";
+		}
+	}
 	void max() {}
 	void mean() {}
 	void median() {}
@@ -97,29 +99,29 @@ public:
 string get_random_number() {
 	random_device dev;
 	mt19937 twister(dev());
-	uniform_int_distribution<int> dist(0, 9);
+	uniform_int_distribution<int32_t> dist(0, 9);
 
 	return to_string(dist(twister));
 }
 
-// Assigns the values of each index of the 2D vector to random values
+// Assigns the values of each index of a 2D vector to random values
 template<typename T>
-void create_random_table(vector<vector<T>> &vectors) {
-	for (size_t i = 0; i < 5; i++) {
+void create_random_table(vector<Column<T>>& vectors) {
+	for (int32_t i = 0; i < 5; i++) {
 		vectors[0][i] = "-";
 	}
-	for (size_t i = 1; i < 5; i++) {
-		for (size_t j = 0; j < 5; j++) {
+	for (int32_t i = 1; i < 5; i++) {
+		for (int32_t j = 0; j < 5; j++) {
 			vectors[i][j] = get_random_number();
 		}
 	}
 }
 
-// Prints the 2D vector
+// Prints a 2D vector
 template<typename T>
-void print_2d_vector(const vector<vector<T>> &vectors) {
-	for (size_t i = 0; i < 5; i++) {
-		for (size_t j = 0; j < 5; j++) {
+void print_2d_vector(const vector<Column<T>>& vectors) {
+	for (int32_t i = 0; i < 5; i++) {
+		for (int32_t j = 0; j < 5; j++) {
 			cout << vectors[i][j] << " ";
 		}
 		cout << endl;
@@ -127,11 +129,11 @@ void print_2d_vector(const vector<vector<T>> &vectors) {
 }
 
 int main() {
-	vector<vector<string>> vectors(5, std::vector<string>(5));
+	vector<Column<string>> vectors(5, Column<string>(5));
 
 	create_random_table(vectors);
 
-	DataFrame<string> df(vectors);
+	DataFrame<Column, string> df(vectors);
 
 	print_2d_vector(vectors);
 
@@ -140,6 +142,6 @@ int main() {
 
 	print_2d_vector(df.get_dataframe());
 
-	cout << df[0].min() << endl;
-
+	cout << "MIN:\n";
+	df[1].min();
 }
